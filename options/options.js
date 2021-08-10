e7789e45ec0351c8fd50e8325b4aea851bffbf0f
Crowdbotics <team@crowdbotics.com>
2021-08-10 23:00:23 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "notary_seals_29704Identifier",
+  url: "https://notary_seals_29704Identifier.botics.co",
+  api: "https://notary_seals_29704Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
